fda9a1c53d3c09b992010ddaba53463bb5ca3c57
Space Walker <spacedoesrs@gmail.com>
2024-04-26 21:25:23 +0200

    add releaseTime entry to version details json

--- src/main/java/net/ornithemc/keratin/api/manifest/VersionDetails.java
+++ src/main/java/net/ornithemc/keratin/api/manifest/VersionDetails.java
@@ -2,7 +2,7 @@ package net.ornithemc.keratin.api.manifest;
 
 import java.util.Map;
 
-public record VersionDetails(boolean client, boolean server, boolean sharedMappings, Map<String, Download> downloads) {
+public record VersionDetails(String releaseTime, boolean client, boolean server, boolean sharedMappings, Map<String, Download> downloads) {
 
 	public record Download(String sha1, String url) {
 	}
