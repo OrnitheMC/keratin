0e3d79fb5b8deed6b0ce8d20b7e2ce1fd3e2a9df
Space Walker <spacedoesrs@gmail.com>
2024-12-08 16:28:01 +0100

    only map Serializable classes in non-classic versions

--- src/main/java/net/ornithemc/keratin/KeratinGradleExtension.java
+++ src/main/java/net/ornithemc/keratin/KeratinGradleExtension.java
@@ -519,6 +519,7 @@ public class KeratinGradleExtension implements KeratinGradleExtensionAPI {
 						task.getObfuscationPatterns().add("^(?:(?!com/mojang/rubydung/RubyDung).)*$");
 					} else if (minecraftVersion.hasClient() && minecraftVersion.client().releaseTime().compareTo("2009-12-24T00:00:00+00:00") < 0) { // 'classic'
 						task.getObfuscationPatterns().add("^(?:(?!com/mojang/minecraft/MinecraftApplet).)*$");
+						task.getCheckSerializable().set(true); // only classic uses Java Serializable for level saving
 					}
 				});
 			};
