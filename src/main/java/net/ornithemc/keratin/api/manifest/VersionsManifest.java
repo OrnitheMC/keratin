222e377825991bc45bf804dff0675529163f9b22
Space Walker <spacedoesrs@gmail.com>
2025-10-17 16:57:11 +0200

    switch to Ornithe manifests

--- src/main/java/net/ornithemc/keratin/api/manifest/VersionsManifest.java
+++ src/main/java/net/ornithemc/keratin/api/manifest/VersionsManifest.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 public record VersionsManifest(List<Entry> versions) {
 
-	public record Entry(String id, String url, String details) {
+	public record Entry(String id, String url, String sha1, String details, String detailsSha1) {
 	}
 
 	public Optional<Entry> find(String version) {
